003823397d639d264d5b8c7e2c78631793136a8a
NavidFarahmand <NavidFarahmand75@gmail.com>
2019-03-16 16:22:49 +0330

    minor improvment

--- configs/router.js
+++ configs/router.js
@@ -4,7 +4,7 @@ var router = {
 add:function(app){
   var path = require('path');
   // app.use(this.prefix+'/user',require(this.controllers+'UserController'));
-  app.use(this.prefix+'/appPageContent',require(path.join(this.controllers,'App','PageContentController')));
+  app.use(this.prefix+'/app/pageContent',require(path.join(this.controllers,'App','PageContentController')));
 }
 };
 module.exports = router;
