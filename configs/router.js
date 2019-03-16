1d0fb09a66b92ac951f880d25e4f2151bb10bce6
NavidFarahmand <NavidFarahmand75@gmail.com>
2019-03-16 16:21:39 +0330

    add app pagecontent controller and it's route

--- configs/router.js
+++ configs/router.js
@@ -2,7 +2,9 @@ var router = {
  controllers : "../controllers/",
  prefix : "/api",
 add:function(app){
+  var path = require('path');
   // app.use(this.prefix+'/user',require(this.controllers+'UserController'));
+  app.use(this.prefix+'/appPageContent',require(path.join(this.controllers,'App','PageContentController')));
 }
 };
 module.exports = router;
