99f53544140697e35f01c4dc811be1344e7f1528
NavidFarahmand <NavidFarahmand75@gmail.com>
2019-03-16 14:38:13 +0330

    add compression module and solved serving problem

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 // load Modules
 var express = require('express');
+var compression = require('compression');
 var app = express();
 var path = require('path');
 
@@ -7,8 +8,12 @@ var path = require('path');
 var serverConfig = require('./configs/server');
 var router = require('./configs/router');
 // enable configs
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(compression());
 router.add(app);
+app.use(express.static("/home/navidfarahmand/Projects/Angular/SecurityTester/dist/SecurityTester"));
+app.use("*",express.static("/home/navidfarahmand/Projects/Angular/SecurityTester/dist/SecurityTester/index.html"));
+// app.use(express.static(path.join(__dirname, "public")));
+// app.use("*",express.static(path.join(__dirname, "public","index.html")));
 // start Server
 app.listen(serverConfig.portNumber,function()
 {
