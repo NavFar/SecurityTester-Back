20ba61dd88fe989528b55b84a77af8a4a6f9de31
NavidFarahmand <NavidFarahmand75@gmail.com>
2019-03-17 02:46:31 +0330

    enable https server

--- main.js
+++ main.js
@@ -1,12 +1,16 @@
 // load Modules
+var fs = require('fs');
 var express = require('express');
 var compression = require('compression');
 var app = express();
 var path = require('path');
-
+var https = require('https');
 // load configs
 var serverConfig = require('./configs/server');
 var router = require('./configs/router');
+var privateKey  = fs.readFileSync('./configs/https/domain.key', 'utf8');
+var certificate = fs.readFileSync('./configs/https/domain.crt', 'utf8');
+var credentials = {key: privateKey, cert: certificate};
 // enable configs
 app.use(compression());
 router.add(app);
@@ -15,7 +19,8 @@ app.use("*",express.static("/home/navidfarahmand/Projects/Angular/SecurityTester
 // app.use(express.static(path.join(__dirname, "public")));
 // app.use("*",express.static(path.join(__dirname, "public","index.html")));
 // start Server
-app.listen(serverConfig.portNumber,function()
+var httpsServer= https.createServer(credentials,app);
+httpsServer.listen(serverConfig.portNumber,function()
 {
   console.log("Web Server is Listening to Port "+serverConfig.portNumber);
 });
