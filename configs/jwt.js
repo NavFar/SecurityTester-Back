daa06c10460b1c2d814568ff37ba4bad994eda27
NavidFarahmand <NavidFarahmand75@gmail.com>
2019-03-18 00:49:27 +0330

    add expire time to config

--- configs/jwt.js
+++ configs/jwt.js
@@ -2,5 +2,6 @@ var path = require('path');
 var jwt ={
   public:path.join(__dirname,"jwt/public_key.pem"),
   private:path.join(__dirname,"jwt/private_key.pem"),
+  expire:86400 ,//in seconds
 };
 module.exports = jwt;
