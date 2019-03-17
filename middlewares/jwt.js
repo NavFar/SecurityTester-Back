17d71c6684c76a1ad16c0c0caa3ede8cdb8bebc5
NavidFarahmand <NavidFarahmand75@gmail.com>
2019-03-18 00:48:37 +0330

    add admin jwt detector middleware

---
+++ middlewares/jwt.js
@@ -0,0 +1,38 @@
+module.exports = function(req,res,next)
+{
+var fs= require('fs');
+var express = require('express');
+var router = express.Router();
+var bodyParser = require('body-parser');
+var User = require('../models/User')
+var jwt = require('jsonwebtoken');
+var jwtConfig = require('../configs/jwt');
+var publicKey = fs.readFileSync(jwtConfig.public);
+var token = req.headers['authorization'];
+if(token){
+var place = token.indexOf(" ");
+if(place!=-1)
+  token = token.substring(place+1);
+}else {
+  token=null;
+}
+if(token)
+{
+  jwt.verify(token, publicKey,{ algorithm: 'RS256' }, function(err, decoded) {
+    if(!err)
+    {
+      User.findById(decoded.id, function (err, user) {
+        if(!err&&user)
+        {
+          res.locals.user=user;
+        }
+          next();
+      });
+    }
+    else
+      res.status(401).send();
+  });
+}
+else
+  res.status(401).send();
+}
