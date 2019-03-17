48743b0027fa0af71904efca1ca6c61521c40340
NavidFarahmand <NavidFarahmand75@gmail.com>
2019-03-18 01:55:48 +0330

    add site-data route

--- configs/router.js
+++ configs/router.js
@@ -7,9 +7,13 @@ var router = {
   var path = require('path');
   app.use(this.prefix+'/app/pageContent',require(path.join(this.controllers,this.app,"PageContent",'PageContentController')));
   /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //add middleware to handle jwt
   var jwtMiddleware = require('../middlewares/jwt.js');
   app.use(this.prefix+'/admin',jwtMiddleware)
+  //actual routes
   app.use(this.prefix+'/admin',require(path.join(this.controllers,this.admin,"UserController")));
+  app.use(this.prefix+'/admin/siteData/',require(path.join(this.controllers,this.admin,"siteDataController")));
+
   }
 };
 module.exports = router;
