60516d20f646cf5da9383e4600d45549f37890b2
NavidFarahmand <NavidFarahmand75@gmail.com>
2019-03-17 22:02:30 +0330

    minor improvment

--- main.js
+++ main.js
@@ -5,21 +5,30 @@ var compression = require('compression');
 var app = express();
 var path = require('path');
 var https = require('https');
+var mongoose = require('mongoose');
 // load configs
 var serverConfig = require('./configs/server');
 var router = require('./configs/router');
-var privateKey  = fs.readFileSync('./configs/https/domain.key', 'utf8');
-var certificate = fs.readFileSync('./configs/https/domain.crt', 'utf8');
+var httpsConfigs = require('./configs/https');
+var privateKey  = fs.readFileSync(httpsConfigs.key, 'utf8');
+var certificate = fs.readFileSync(httpsConfigs.certification, 'utf8');
 var credentials = {key: privateKey, cert: certificate};
+var dbConfig = require('./configs/db.js');
 // enable configs
+// enable compression
 app.use(compression());
+// add api routes
 router.add(app);
+//connect to database
+mongoose.connect('mongodb://'+dbConfig.url+':'+dbConfig.port+'/'+dbConfig.name,{ useNewUrlParser: true });
+//serve front-end files
 app.use(express.static("/home/navidfarahmand/Projects/Angular/SecurityTester/dist/SecurityTester"));
 app.use("*",express.static("/home/navidfarahmand/Projects/Angular/SecurityTester/dist/SecurityTester/index.html"));
 // app.use(express.static(path.join(__dirname, "public")));
 // app.use("*",express.static(path.join(__dirname, "public","index.html")));
-// start Server
+//Enable https for server
 var httpsServer= https.createServer(credentials,app);
+// start Server
 httpsServer.listen(serverConfig.portNumber,function()
 {
   console.log("Web Server is Listening to Port "+serverConfig.portNumber);
