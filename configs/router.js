1cc619c211ca3a07a83dce6b13e0966b1b167370
NavidFarahmand <NavidFarahmand75@gmail.com>
2019-03-20 03:22:26 +0330

    add uploader route

--- configs/router.js
+++ configs/router.js
@@ -2,18 +2,20 @@ var router = {
  controllers : "../controllers",
  app:"App",
  admin:"Admin",
- prefix : "/api",
+ apiPrefix : "/api",
+ adminPrefix: "/admin",
  add:function(app){
   var path = require('path');
-  app.use(this.prefix+'/app/pageContent',require(path.join(this.controllers,this.app,"PageContent",'PageContentController')));
-  app.use(this.prefix+'/app/siteData',require(path.join(this.controllers,this.app,'SiteDataController')));
+  app.use(this.apiPrefix+'/app/pageContent',require(path.join(this.controllers,this.app,"PageContent",'PageContentController')));
+  app.use(this.apiPrefix+'/app/siteData',require(path.join(this.controllers,this.app,'SiteDataController')));
   /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
   //add middleware to handle jwt
   var jwtMiddleware = require('../middlewares/jwt.js');
-  app.use(this.prefix+'/admin',jwtMiddleware)
+  app.use(this.apiPrefix+this.adminPrefix,jwtMiddleware)
   //actual routes
-  app.use(this.prefix+'/admin',require(path.join(this.controllers,this.admin,"UserController")));
-  app.use(this.prefix+'/admin/siteData/',require(path.join(this.controllers,this.admin,"SiteDataController")));
+  app.use(this.apiPrefix+this.adminPrefix,require(path.join(this.controllers,this.admin,"UserController")));
+  app.use(this.apiPrefix+this.adminPrefix+'/siteData/',require(path.join(this.controllers,this.admin,"SiteDataController")));
+  app.use(this.apiPrefix+this.adminPrefix+'/uploader/',require(path.join(this.controllers,this.admin,"UploaderController")));
 
   }
 };
