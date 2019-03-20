3eeb7071c47693810702420cb321937c6a2d7b90
NavidFarahmand <NavidFarahmand75@gmail.com>
2019-03-20 14:59:54 +0330

    add test controller

--- configs/router.js
+++ configs/router.js
@@ -8,6 +8,7 @@ var router = {
   var path = require('path');
   app.use(this.apiPrefix+'/app/pageContent',require(path.join(this.controllers,this.app,"PageContent",'PageContentController')));
   app.use(this.apiPrefix+'/app/siteData',require(path.join(this.controllers,this.app,'SiteDataController')));
+  app.use(this.apiPrefix+'/app/siteData',require(path.join(this.controllers,this.app,'TestController')));
   /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
   //add middleware to handle jwt
   var jwtMiddleware = require('../middlewares/jwt.js');
@@ -16,7 +17,6 @@ var router = {
   app.use(this.apiPrefix+this.adminPrefix,require(path.join(this.controllers,this.admin,"UserController")));
   app.use(this.apiPrefix+this.adminPrefix+'/siteData/',require(path.join(this.controllers,this.admin,"SiteDataController")));
   app.use(this.apiPrefix+this.adminPrefix+'/uploader/',require(path.join(this.controllers,this.admin,"UploaderController")));
-
   }
 };
 module.exports = router;
---
+++ controllers/App/TestController.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var fs = require('fs');
+var path = require('path');
+var router = express.Router();
+router.post("/new",(req,res)=>{
+  
+});
+module.exports = router;
