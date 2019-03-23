d6ec0228989bd4e94bf321fedacfcaf3d278fe62
NavidFarahmand <NavidFarahmand75@gmail.com>
2019-03-24 01:25:28 +0430

    add directory address

--- configs/server.js
+++ configs/server.js
@@ -2,5 +2,7 @@ var path = require('path');
 var server={
   portNumber:3000,
   publicDirectoryLocation:path.join(__dirname,"../public"),
+  serverDirectoryLocation:path.join(__dirname,"../"),
+
 };
 module.exports = server;
