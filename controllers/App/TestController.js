56c97d800680ed687fd7ac0f813502880988c81c
NavidFarahmand <NavidFarahmand75@gmail.com>
2019-03-24 18:30:01 +0430

    move get Result to it's controller

--- controllers/App/TestController.js
+++ controllers/App/TestController.js
@@ -2,7 +2,38 @@ var express = require('express');
 var fs = require('fs');
 var path = require('path');
 var router = express.Router();
+var recaptchaChecker = require('../Shared/RecaptchaChecker');
+var requestModel = require('../../models/Request');
+var scriptConfig = require("../../configs/script");
+var queues = require('../../queue/queues');
+
 router.post("/new",(req,res)=>{
-  
+  if(typeof req.body.expose!="boolean"||typeof req.body.address!="string"
+  // ||typeof req.body.recaptcha!="string"
+)
+      return res.status(400).send();
+  recaptchaChecker(req.body.recaptcha,
+        function(){
+          var urlRegex = RegExp(/^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/gi);
+          var isAddress = urlRegex.test(req.body.address)
+          var ipRegex = require('ip-regex');
+          var isIp=ipRegex({exact: true}).test(req.body.address);
+          if((isAddress && isIp)||(!isAddress && !isIp))
+            return res.status(400).send();
+          requestModel.create({
+            url:req.body.address,
+            pendingOn:2,
+            results:""
+          },function(err,request){
+            if(err)
+              return res.status(500).send();
+            queues.addToAll(request._id,req.body.address);
+            // global.socket[req.query.id]=[];
+            return res.status(200).json(request._id);
+          });
+        },function(){
+          return res.status(503).send();
+        });
 });
+
 module.exports = router;
