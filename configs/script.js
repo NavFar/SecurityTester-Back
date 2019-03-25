a0e804577c01e2dec3981b4107af041577af6e56
NavidFarahmand <NavidFarahmand75@gmail.com>
2019-03-25 22:55:40 +0430

    add desc

--- configs/script.js
+++ configs/script.js
@@ -2,8 +2,8 @@ var path = require('path');
 var scriptsPrefix = "../scripts/";
 var script={
     scripts:{
-      observatory:{name: "observatory" ,scriptFile: path.join(__dirname,scriptsPrefix,"observatory/observatory")},
-      // testssl:{name:"testssl" ,scriptFile: path.join(__dirname,scriptsPrefix,"testssl/testssl")},
+      observatory:{name: "observatory" ,scriptFile: path.join(__dirname,scriptsPrefix,"observatory/observatory"),describtion:"Check Somethingss"},
+      // testssl:{name:"testssl" ,scriptFile: path.join(__dirname,scriptsPrefix,"testssl/testssl"),describtion:""},
     },
 };
 module.exports = script;
