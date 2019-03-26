c6ff1346e3290000bc98cf51a558bdcdc2ab8853
NavidFarahmand <NavidFarahmand75@gmail.com>
2019-03-26 09:52:58 +0430

    mior fixes

--- controllers/App/ResultController.js
+++ controllers/App/ResultController.js
@@ -4,31 +4,44 @@ var path = require('path');
 var router = express.Router();
 var requestModel = require('../../models/Request');
 router.post("/",(req,res)=>{
-  if(!req.body.id.match(/^[0-9a-fA-F]{24}$/))
+  if(!req.body.id||!req.body.id.match(/^[0-9a-fA-F]{24}$/))
     return res.status(400).send();
   requestModel.findOne({_id:req.body.id},function(err,request){
     if(err)
-      return res.status(500).json(err);
+      return res.status(500).send();
     if(!request)
       return res.status(404).send();
-    res.status(200).json((request.results));
+    res.status(200).json((request));
   });
 });
+
+router.post("/recent",(req,res)=>{
+  requestModel.find({expose:true,pending:false}).limit(20).sort({end:1}).select({_id:1,url:1,end:1}).exec(
+    (err,requests)=>
+    {
+      if(err)
+        return res.status(500).send();
+      res.status(200).json(requests);
+    }
+  );
+});
+
 router.ws("/progress",function(ws, req) {
   if(!req.query.id){
-    console.log("1");
     return ws.terminate();
   }
     requestModel.findOne({_id:req.query.id},function(err,request){
       if(err){
-        console.log("2");
         return ws.terminate();
       }
       if(!request){
-        console.log("3");
         return ws.terminate();
       }
-      // global.socket[req.query.id].push(ws)
+      if(!request.pending){
+        return ws.terminate();
+      }
+      if(global.socket[req.query.id])
+      global.socket[req.query.id].push(ws)
       });
 });
 module.exports = router;
--- controllers/App/TestController.js
+++ controllers/App/TestController.js
@@ -8,12 +8,12 @@ var scriptConfig = require("../../configs/script");
 var queues = require('../../queue/queues');
 
 router.post("/new",(req,res)=>{
-  if(typeof req.body.expose!="boolean"||typeof req.body.address!="string"
-  // ||typeof req.body.recaptcha!="string"
-)
+  if(typeof req.body.expose!="boolean"||typeof req.body.address!="string"||typeof req.body.recaptcha!="string")
       return res.status(400).send();
   recaptchaChecker(req.body.recaptcha,
         function(){
+          return res.status(503).send();
+        },function(){
           var urlRegex = RegExp(/^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/gi);
           var isAddress = urlRegex.test(req.body.address)
           var ipRegex = require('ip-regex');
@@ -22,17 +22,23 @@ router.post("/new",(req,res)=>{
             return res.status(400).send();
           requestModel.create({
             url:req.body.address,
-            pendingOn:2,
-            results:""
+            start:new Date(),
+            pending:true,
+            pendingOn:[],
+            expose:!req.body.expose,
+            results:[]
           },function(err,request){
             if(err)
               return res.status(500).send();
-            queues.addToAll(request._id,req.body.address);
-            // global.socket[req.query.id]=[];
-            return res.status(200).json(request._id);
+            let tests = queues.addToAll(request._id,req.body.address);
+            global.socket[request._id]=[];
+            request.pendingOn = tests;
+            request.save(function(err,newRequest){
+              if(err)
+                return res.status(500).send();
+              return res.status(200).json(request._id);
+            });
           });
-        },function(){
-          return res.status(503).send();
         });
 });
 
