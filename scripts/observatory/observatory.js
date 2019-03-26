1b897c66f2c06343b06f2e8451a412ec1ac5f31d
NavidFarahmand <NavidFarahmand75@gmail.com>
2019-03-26 09:51:35 +0430

    minor fixes

--- scripts/observatory/observatory.js
+++ scripts/observatory/observatory.js
@@ -2,34 +2,37 @@ var observatory = function(job,done){
   const { exec } = require('child_process');
   exec('observatory '+job.data.url+' --format=json --zero --rescan --quiet', (error, stdout, stderr) => {
     var result={
-      name:"Webserver Security",
+      // name:job.data.name,
       error:{
         exist:false,
         desc:"",
         code:-1
       },
       parts:[],
-      overal:''
+      // overal:''
+      pass:false
     };
+  var overalScore = 0;
    if (error) {
      result.error.exist=true;
-     result.error.desc="Observatory can't start "+error;
+     // result.error.desc="Observatory can't start";
+     result.error.desc="";
      result.error.code=1;
    }
     else if(stderr)
    {
      result.error.exist=true;
-     result.error.desc="Observatory can't work properly";
+     // result.error.desc="Observatory can't work properly";
+     result.error.desc="";
      result.error.code=2;
    }
    else{
    var jsonOutput = JSON.parse(stdout);
    var allKeys = Object.keys(jsonOutput);
-   var overalScore = 'F';
 
    for (var i=0;i<allKeys.length;i++)
    {
-     overalScore = jsonOutput[allKeys[i]].score_modifier;
+     overalScore += jsonOutput[allKeys[i]].score_modifier;
      var currentPart={
        name:jsonOutput[allKeys[i]].name,
        pass:jsonOutput[allKeys[i]].pass,
@@ -38,33 +41,23 @@ var observatory = function(job,done){
      }
      result.parts.push(currentPart);
    }
-   result.overal='F';
+
    if(overalScore>=85)
-     result.overal = 'A';
+     result.pass =true;
    else if(overalScore>=70)
-     result.overal = 'B';
+     result.pass =true;
    else if(overalScore>=55)
-     result.overal = 'C';
+     result.pass =false;
    else if(overalScore>=40)
-     result.overal = 'D';
+     result.pass =false;
    else if(overalScore>=25)
-     result.overal = 'E';
-
+     result.pass =false;
  }
- var requestModel = require('../../models/Request');
- // console.log(result)
- requestModel.findOne({_id:job.data.id},
-   function(err,request){
-     // console.log(job.data.id)
-     request.results +=JSON.stringify(result);
-     request.save(function(err,newRequest){
-
-     });
- });
- // for(let i=0;i<global.socket[job.data.id].length;i++)
- // {
- //   global.socket[job.data.id][i].send("Ki")
- // }
+  var afterDone = require('../afterDone');
+  if(result.error.exist)
+    afterDone(job.data.id,"",job.data.name,-1,result);
+  else
+    afterDone(job.data.id,overalScore/5,job.data.name,1,result);
  return done(null)
  });
 }
