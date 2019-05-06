74e32681da626cea7c8a9b5ed0d973bddfb1a8cf
NavidFarahmand <NavidFarahmand75@gmail.com>
2019-05-06 12:30:38 +0430

    fix error

--- configs/server.js
+++ configs/server.js
@@ -1,8 +1,7 @@
 var path = require('path');
 var server={
   portNumber:443,
-  // publicDirectoryLocation:path.join(__dirname,"../public"),
-  publicDirectoryLocation:"/home/navidfarahmand/Projects/Angular/SecurityTester/dist/SecurityTester",
+  publicDirectoryLocation:path.join(__dirname,"../public"),
   serverDirectoryLocation:path.join(__dirname,"../"),
 };
 module.exports = server;
